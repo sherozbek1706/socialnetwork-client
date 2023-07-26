23437d0bf2635e90377dce494ede3ca0072e662b
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-07-26 11:37:56 +0500

    navbar let side

--- src/layout/navbar/navbar.jsx
+++ src/layout/navbar/navbar.jsx
@@ -1,23 +1,31 @@
 import "./navbar.css";
-
+import { AiFillHome, AiOutlineUserAdd } from "react-icons/ai";
+import { HiDocumentText } from "react-icons/hi";
+import { FaUsers } from "react-icons/fa";
+import { BiSolidLogIn } from "react-icons/bi";
 export const Navbar = () => {
   return (
     <div className="Navbar">
       <h1 className="Navbar__logo">My Projects</h1>
       <div className="Navbar__nav">
         <a href="#" className="Navbar__link">
+          <AiFillHome className="nav__icon" />
           Home
         </a>
         <a href="#" className="Navbar__link">
+          <HiDocumentText className="nav__icon" />
           Posts
         </a>
         <a href="#" className="Navbar__link">
+          <FaUsers className="nav__icon" />
           Users
         </a>
         <a href="#" className="Navbar__link">
+          <BiSolidLogIn className="nav__icon" />
           Login
         </a>
         <a href="#" className="Navbar__link">
+          <AiOutlineUserAdd className="nav__icon" />
           Register
         </a>
       </div>
