4ee0b63ab8414ed2a1b80f8feb9ed037fdab6fde
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-08-05 20:01:03 +0500

    body background qo'ydim #020209

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import { Login, Main, Register } from "./pages/";
 
 export const App = () => {
   return (
-    <Router>
-      <Route exact path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route
-        path="/users"
-        element={
-          <h1>
-            <Link to="/">HOME</Link>
-          </h1>
-        }
-      />
-    </Router>
+    <div className="general">
+      <Router>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/users"
+          element={
+            <h1>
+              <Link to="/">HOME</Link>
+            </h1>
+          }
+        />
+      </Router>
+    </div>
   );
 };
