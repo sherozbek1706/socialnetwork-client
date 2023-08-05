3fee5b605001f4d173805a698f90eef61947c9de
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-08-05 20:40:06 +0500

    form-crete-post ochib oldim

---
+++ src/components/form-create-post/form-create-post.jsx
@@ -0,0 +1,8 @@
+import "./form-create-post.css";
+export const FormCreatePost = () => {
+  return (
+    <div className="FormCreatePost">
+      <h1>form-create-post</h1>
+    </div>
+  );
+};
--- src/pages/createPost/create-post.jsx
+++ src/pages/createPost/create-post.jsx
@@ -1,32 +1,11 @@
+import { FormCreatePost } from "../../components";
+import { Navbar } from "../../layout";
 import "./create-post.css";
 export const CreatePost = () => {
   return (
-    <div>
-      <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum voluptatum
-        numquam laborum eius ut, quo, ducimus tenetur vero dignissimos quisquam
-        minus sed corrupti pariatur, vitae necessitatibus officiis reprehenderit
-        soluta optio expedita mollitia incidunt aspernatur velit adipisci?
-        Voluptatibus cumque accusantium neque? Quibusdam ex voluptatibus
-        perspiciatis optio totam laudantium quia reiciendis itaque quasi.
-        Perspiciatis, eum possimus dolorum labore veniam, doloribus accusamus
-        repellendus ex veritatis, reprehenderit vero? Iste aspernatur nostrum
-        consequatur consequuntur sequi accusamus! Dolores nam quam voluptatum
-        ullam vero nobis sapiente. Blanditiis hic exercitationem non nulla ad
-        beatae quibusdam magnam veritatis cumque consequatur dolore rerum, modi
-        a commodi repellat sint corporis esse aspernatur ab, illo totam itaque
-        nemo. Illo quos recusandae repellendus magni consectetur aliquid
-        aperiam, perferendis necessitatibus sunt neque doloremque nulla qui
-        culpa animi saepe incidunt blanditiis similique eligendi nisi, officia,
-        excepturi impedit! Modi quia quod delectus inventore, eum omnis
-        accusantium cum velit dicta consequuntur iste temporibus sequi numquam
-        ut reprehenderit voluptate voluptas. Natus incidunt esse tenetur
-        architecto! Quam doloremque vitae error ex expedita similique minus
-        labore. Vel quod quia laborum repudiandae ipsam, dolore fugiat? Nihil at
-        inventore velit sapiente cupiditate, eaque quo dolorum modi voluptate
-        perspiciatis! Repellat incidunt quidem illo nemo cupiditate praesentium
-        fugiat! Vero ducimus tempora repudiandae accusantium ullam!
-      </h1>
+    <div className="CreatePost">
+      <Navbar />
+      <FormCreatePost />
     </div>
   );
 };
