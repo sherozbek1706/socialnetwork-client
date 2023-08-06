15c5562b63bb639a612ebe0aa59414ad95064555
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-08-06 17:36:07 +0500

    profilega api uladim

--- src/components/profile-user/profile-user.jsx
+++ src/components/profile-user/profile-user.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import { ProfileUserInfo } from "../../layout";
+import { axiosInstance } from "../../services";
 import "./profile-user.css";
 export const ProfileUser = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axiosInstance
+      .get("/users/me")
+      .then((data) => {
+        console.log(data.data.data);
+        return setData(data.data.data)
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="ProfileUser">
-      <ProfileUserInfo />
+      <ProfileUserInfo data={data} />
     </div>
   );
 };
--- src/layout/profile-user-info/profile-user-info.jsx
+++ src/layout/profile-user-info/profile-user-info.jsx
@@ -1,8 +1,14 @@
 import "./profile-user-info.css";
-export const ProfileUserInfo = () => {
+export const ProfileUserInfo = ({ data }) => {
   return (
     <div className="ProfileUserInfo">
-      <h1 className="ProfileUserInfo__title">Profile <span>#sherozbek.17</span></h1>
+      <h1 className="ProfileUserInfo__title">
+        Profile <span>#{data.username}</span>
+      </h1>
+      
+      <p className="ProfileUserInfo__fullname">
+        {data.first_name} {data.last_name}
+      </p>
     </div>
   );
 };
