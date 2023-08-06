60571d9b7eb5f901764addc5afe51d281beac2c9
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-08-06 23:14:45 +0500

    profile page ozroq chala bitdi

--- src/layout/profile-user-info/profile-user-info.jsx
+++ src/layout/profile-user-info/profile-user-info.jsx
@@ -1,14 +1,34 @@
 import "./profile-user-info.css";
+import { api } from "../../shared/url.js";
+import { BiEdit } from "react-icons/bi";
+import { FaStar } from "react-icons/fa";
 export const ProfileUserInfo = ({ data }) => {
   return (
     <div className="ProfileUserInfo">
       <h1 className="ProfileUserInfo__title">
         Profile <span>#{data.username}</span>
       </h1>
-      
-      <p className="ProfileUserInfo__fullname">
-        {data.first_name} {data.last_name}
-      </p>
+      <div className="ProfileUserInfo__user">
+        <img
+          className="ProfileUserInfo__photo"
+          src={`${api}${data.image}`}
+          alt=""
+        />
+        <div className="ProfileUserInfo__userinfo">
+          <p className="ProfileUserInfo__fullname">
+            {data.first_name} {data.last_name}
+          </p>
+          <p className="ProfileUserInfo__email">{data.email}</p>
+          <div className="ProfileUserInfo__options">
+            <button className="ProfileUserInfo__select">
+              <BiEdit className="ProfileUserInfo__selecticon" /> EDIT ACCOUNT
+            </button>
+            <button className="ProfileUserInfo__select">
+              <FaStar className="ProfileUserInfo__selecticon" /> GET STAR
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
